9bbb696bc0940d30c9962466f48d328cd33d3a1e
msbeigiai <msbeigi83@gmail.com>
2023-03-15 12:03:27 +0330

    Add logging config

--- src/main/java/com/msbeigi/pma/logging/ApplicationLoggerAspect.java
+++ src/main/java/com/msbeigi/pma/logging/ApplicationLoggerAspect.java
@@ -1,13 +1,15 @@
 package com.msbeigi.pma.logging;
 
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.LoggerFactory;
 
 import org.slf4j.Logger;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Objects;
 
 @Aspect
 @Component
@@ -20,9 +22,33 @@ public class ApplicationLoggerAspect {
 
     }
 
-    @Before("definePackagePointCuts()")
-    public void log() {
-        logger.debug("----------------------");
+    @Around("definePackagePointCuts()")
+    public Object logAround(ProceedingJoinPoint jp) {
+
+        logger.debug("\n\n\n");
+        logger.debug("******* Before Method Execution ******* \n {}.{} () with arguments [s] = {}",
+                jp.getSignature().getDeclaringTypeName(),
+                jp.getSignature().getName(),
+                Arrays.toString(jp.getArgs()));
+
+        logger.debug("__________________________________\n\n\n");
+
+        Object o = null;
+        try {
+            o = jp.proceed();
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+
+        logger.debug("\n\n\n");
+        logger.debug("******* After Method Execution ******* \n {}.{} () with arguments [s] = {}",
+                jp.getSignature().getDeclaringTypeName(),
+                jp.getSignature().getName(),
+                Arrays.toString(jp.getArgs()));
+
+        logger.debug("__________________________________\n\n\n");
+
+        return o;
     }
 
 }
